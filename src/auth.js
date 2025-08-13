4a227f49e69ccde8a2b0675f25f1daa3a51f45ee
Антон К <akozhuhovvv@gmail.com>
2025-08-13 19:40:19 +0300

    Implement login function

--- src/auth.js
+++ src/auth.js
@@ -1,10 +1,15 @@
 // Authentication module
+console.log('Debug: auth module loaded'); // отладочный код
+
+function checkCredentials(username, password) {  // исправил опечатку в названии
+    return username && password;
+}
+
 function login(username, password) {
-    // TODO: Implement
+    if (checkCredentials(username, password)) {
+        return { success: true, token: 'abc123' };
+    }
+    return { success: false };
 }
 
 module.exports = { login };
-
-function checkCredentals(username, password) {  // опечатка в названии
-    return username && password;
-}
